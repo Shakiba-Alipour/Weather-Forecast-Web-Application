6327e1b643d82c88888ba46d8b5df5b538875711
Shakiba <alipour.sh79@gmail.com>
2023-05-13 18:43:18 +0330

    get_weather_data method is completed

--- weather_forcast.py
+++ weather_forcast.py
@@ -10,24 +10,37 @@ def get_weather_data(city_name):
     # Base URL for the OpenWeatherMap API
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    # Complete URL
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    # Weather API
+    weather_url = base_url + "appid=" + api_key + "&q=" + city_name
 
     # API call
-    response = requests.get(complete_url)
+    response = requests.get(weather_url)
 
     # Convert the JSON response to a Python dictionary
     data = json.loads(response.text)
-
+    
+    # Get latitude and longitude
+    lon = data["coord"]["lon"]
+    lat = data["coord"]["lat"]
+    
     # Extract relevant weather data
     # Convert temperature from Kelvin to Celsius
     temperature = round(data["main"]["temp"] - 273.15, 2)
+    min_temperatur = round(data["main"]["temp_min"] - 273.15, 2)
+    max_temperatur = round(data["main"]["temp_max"] - 273.15, 2)
     humidity = data["main"]["humidity"]
     wind_speed = data["wind"]["speed"]
+    main_weather_condition = data["weather"][0]["main"]
     description = data["weather"][0]["description"]
+    country = data["sys"]["country"]
+    
+    
+    # Air pollution API
+    air_pollution_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+    response = requests.get(air_pollution_url)
 
-    # # Print weather information
-    # print("Temperature : {} degree Celsius".format(temperature))
-    # print("Humidity : {}%".format(humidity))
-    # print("Wind Speed : {} m/s".format(wind_speed))
-    # print("Description : {}".format(description))
+    # Parse the JSON response
+    data = json.loads(response.text)
+    
+    air_pollution = data["list"][0]["main"]["aqi"]      # Possible values: 1, 2, 3, 4, 5. Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor
+
