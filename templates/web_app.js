a6536db4f126e74e00eb2f26c01a16bb30bdcef5
Shakiba <alipour.sh79@gmail.com>
2023-06-29 01:48:43 +0330

    about popup is added and completed

--- templates/web_app.js
+++ templates/web_app.js
@@ -1,1 +1,10 @@
 // const app = Vue.createApp({});
+document
+  .getElementById("open-about-popup")
+  .addEventListener("click", function () {
+    document.getElementById("about-popup").style.display = "block";
+  });
+
+document.getElementById("close-popup").addEventListener("click", function () {
+  document.getElementById("about-popup").style.display = "none";
+});
