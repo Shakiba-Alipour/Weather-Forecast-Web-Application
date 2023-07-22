3c941a17ec23078f6ee1d3e7cb5bc16c27009e1d
Shakiba <alipour.sh79@gmail.com>
2023-07-22 12:14:09 +0330

    changing the location of the search bar is added

--- static/script.js
+++ static/script.js
@@ -13,20 +13,29 @@ document.getElementById("close-popup").addEventListener("click", function () {
 document.getElementById("search-icon").addEventListener("click", function () {
   var city_name = document.getElementById("search-input").value;
   // Fetch weather data from the server
-  fetch(`https://localhost:5500/weather?city=${encodeURIComponent(city_name)}`)
+  var api_url =
+    "https://localhost:5500/weather?city=$%7BencodeURIComponent(city_name)";
+  fetch(api_url)
     .then((response) => response.json())
     .then((data) => {
-      // const weatherInfo = document.getElementById('weather-info');
-      // const cityElement = document.getElementById('city');
-      // const temperatureElement = document.getElementById('temperature');
-      // const descriptionElement = document.getElementById('description');
+      console.log("Done");
+      // change the location of the search bar
+      const search_bar = document.getElementsByTagName("form");
+      const header = document.getElementById("header");
+      header.appendChild(search_bar);
+      header.style.justifyContent = "space-between";
+      search_bar.style.marginRight = 0;
+      search_bar.style.background = "#fffcf2ff";
+      // const weatherInfo = document.getElementById("weather-info");
+      // const cityElement = document.getElementById("city");
+      // const temperatureElement = document.getElementById("temperature");
+      // const descriptionElement = document.getElementById("description");
 
       // cityElement.textContent = data.city;
-      // temperatureElement.textContent = data.temperature + ' °C';
+      // temperatureElement.textContent = data.temperature + " °C";
       // descriptionElement.textContent = data.description;
 
-      // weatherInfo.style.display = 'block';
-      document.getElementById("open-about-popup").click;
+      // weatherInfo.style.display = "block";
     })
     .catch((error) => {
       alert("Failed to fetch weather data.");
