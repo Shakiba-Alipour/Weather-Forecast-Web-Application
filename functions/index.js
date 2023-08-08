48c84681eb1c68f7c614d2a878d4416e1f07c082
Shakiba <alipour.sh79@gmail.com>
2023-08-08 13:43:16 +0330

    copy previous nodejs code into index.js file

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,48 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+const { spawn } = require("child_process");
+
+// Initialize Express app
+const express = require("express");
+const app = express();
+
+// Allow cross-origin requests (if needed)
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Route to fetch weather data from Python script
+app.get("/weather", async (_req, res) => {
+  try {
+    const pythonProcess = spawn("python", ["weather_forecast.py"]);
+    let weatherData = "";
+
+    pythonProcess.stdout.on("data", (data) => {
+      weatherData += data.toString();
+    });
+
+    pythonProcess.on("close", (code) => {
+      try {
+        const parsedData = JSON.parse(weatherData);
+        res.json(parsedData);
+      } catch (error) {
+        console.error("Error parsing weather data:", error);
+        res.status(500).json({ error: "Failed to fetch weather data" });
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
+
+// Export the Cloud Function
+exports.weather = functions.https.onRequest(app);
