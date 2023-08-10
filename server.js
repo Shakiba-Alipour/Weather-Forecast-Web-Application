e8d0dfd58479c71a2a3714e97f843cca336257ed
Shakiba <alipour.sh79@gmail.com>
2023-08-10 13:02:40 +0330

    copy nodejs content into javascript file

--- server.js
+++ server.js
@@ -49,115 +49,117 @@ https.createServer(options, app).listen(port, () => {
   console.log(`Server is running on https://127.0.0.1:${port}`);
 });
 
-// const functions = require("firebase-functions");
-// const fetch = require("node-fetch");
-// const cors = require("cors");
+/////////////////////////////////////////////////////////////////////////////////////
 
-// // Initialize Express app
-// const express = require("express");
-// const app = express();
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
+const cors = require("cors");
 
-// app.use(cors());
+// Initialize Express app
+const express = require("express");
+const app = express();
 
-// class Weather {
-//   constructor(
-//     city_name,
-//     country_name,
-//     longitude,
-//     latitude,
-//     temperature,
-//     min_temperature,
-//     max_temperature,
-//     humidity,
-//     wind_speed,
-//     main_weather_condition,
-//     description,
-//     air_pollution,
-//     next_five_days,
-//     icon_id
-//   ) {
-//     this.city_name = city_name;
-//     this.country_name = country_name;
-//     this.longitude = longitude;
-//     this.latitude = latitude;
-//     this.temperature = temperature;
-//     this.min_temperatur = min_temperatur;
-//     this.max_temperatur = max_temperatur;
-//     this.humidity = humidity;
-//     this.wind_speed = wind_speed;
-//     this.main_weather_condition = main_weather_condition;
-//     this.description = description;
-//     this.air_pollution = air_pollution; // Possible values: 1, 2, 3, 4, 5. Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor
-//     this.next_five_days = next_five_days;
-//     this.icon_id = icon_id;
-//   }
-// }
+app.use(cors());
 
-// app.get("/weather", async (req, res) => {
-//   try {
-//     const apiKey = "6e10fbb861b606deeab532507ffcb0d7";
-//     const city = req.query.city_name;
+class Weather {
+  constructor(
+    city_name,
+    country_name,
+    longitude,
+    latitude,
+    temperature,
+    min_temperature,
+    max_temperature,
+    humidity,
+    wind_speed,
+    main_weather_condition,
+    description,
+    air_pollution,
+    next_five_days,
+    icon_id
+  ) {
+    this.city_name = city_name;
+    this.country_name = country_name;
+    this.longitude = longitude;
+    this.latitude = latitude;
+    this.temperature = temperature;
+    this.min_temperatur = min_temperatur;
+    this.max_temperatur = max_temperatur;
+    this.humidity = humidity;
+    this.wind_speed = wind_speed;
+    this.main_weather_condition = main_weather_condition;
+    this.description = description;
+    this.air_pollution = air_pollution; // Possible values: 1, 2, 3, 4, 5. Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor
+    this.next_five_days = next_five_days;
+    this.icon_id = icon_id;
+  }
+}
 
-//     const response = await fetch(
-//       `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}`
-//     );
+app.get("/weather", async (req, res) => {
+  try {
+    const apiKey = "6e10fbb861b606deeab532507ffcb0d7";
+    const city = req.query.city_name;
 
-//     const data = await response.json();
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}`
+    );
 
-//     const longitude = data.coord.lon;
-//     const latitude = data.coord.lat;
+    const data = await response.json();
 
-//     const temperature = round(data.main.temp - 273.15, 2);
-//     const min_temperature = round(data.main.temp_min - 273.15, 2);
-//     const max_temperature = round(data.main.temp_max - 273.15, 2);
-//     const humidity = data.main.humidity;
-//     const wind_speed = data.wind.speed;
-//     const main_weather_condition = data.weather[0].main;
-//     const description = data.weather[0].description;
-//     const country_name = data.sys.country;
-//     const icon_id = data.weather[0].id;
+    const longitude = data.coord.lon;
+    const latitude = data.coord.lat;
 
-//     const air_pollution_url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
-//     const air_pollution_response = await fetch(air_pollution_url);
-//     const air_pollution_data = await air_pollution_response.json();
+    const temperature = round(data.main.temp - 273.15, 2);
+    const min_temperature = round(data.main.temp_min - 273.15, 2);
+    const max_temperature = round(data.main.temp_max - 273.15, 2);
+    const humidity = data.main.humidity;
+    const wind_speed = data.wind.speed;
+    const main_weather_condition = data.weather[0].main;
+    const description = data.weather[0].description;
+    const country_name = data.sys.country;
+    const icon_id = data.weather[0].id;
 
-//     const air_pollution = air_pollution_data.list[0].main.aqi;
+    const air_pollution_url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
+    const air_pollution_response = await fetch(air_pollution_url);
+    const air_pollution_data = await air_pollution_response.json();
 
-//     const forecast_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${api_key}`;
-//     const forecast_response = await fetch(forecast_url);
-//     const forecast_data = await forecast_response.json();
+    const air_pollution = air_pollution_data.list[0].main.aqi;
 
-//     const forecast = forecast_data.daily.slice(1, 6).map((day) => {
-//       return {
-//         date: day.dt,
-//         min_temp: day.temp.min,
-//         max_temp: day.temp.max,
-//       };
-//     });
+    const forecast_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${api_key}`;
+    const forecast_response = await fetch(forecast_url);
+    const forecast_data = await forecast_response.json();
 
-//     const weather = new Weather(
-//       city_name,
-//       country_name,
-//       longitude,
-//       latitude,
-//       temperature,
-//       min_temperature,
-//       max_temperature,
-//       humidity,
-//       wind_speed,
-//       main_weather_condition,
-//       description,
-//       air_pollution,
-//       forecast,
-//       icon_id
-//     );
+    const forecast = forecast_data.daily.slice(1, 6).map((day) => {
+      return {
+        date: day.dt,
+        min_temp: day.temp.min,
+        max_temp: day.temp.max,
+      };
+    });
 
-//     res.status(200).json(weather);
-//   } catch (error) {
-//     console.error("Error fetching weather data:", error);
-//     res.status(500).json({ error: "Failed to fetch weather data" });
-//   }
-// });
+    const weather = new Weather(
+      city_name,
+      country_name,
+      longitude,
+      latitude,
+      temperature,
+      min_temperature,
+      max_temperature,
+      humidity,
+      wind_speed,
+      main_weather_condition,
+      description,
+      air_pollution,
+      forecast,
+      icon_id
+    );
 
-// // Export the Cloud Function
-// exports.weather = functions.https.onRequest(app);
+    res.status(200).json(weather);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
+
+// Export the Cloud Function
+exports.weather = functions.https.onRequest(app);
